57251645f0eb4e748aacc07b796b2680adf297a5
chansey <chansey97@126.com>
2020-10-29 19:05:40 +0800

    Symbola's font is too small to display ⟦⟧, so adjust size to 16.

--- init.el
+++ init.el
@@ -185,7 +185,7 @@
 ;; Symbola include many Mathematical Alphanumeric Symbols, but windows has no this font by default.
 ;; So install it, if use windows. https://fontlibrary.org/en/font/symbola
 (set-fontset-font "fontset-default"
-                  'symbol (font-spec :family "Symbola"))
+                  'symbol (font-spec :family "Symbola" :size 16))
 
 ;;----------------------------------------------------------------------------
 ;; Displaying Line Numbers and Column Number
