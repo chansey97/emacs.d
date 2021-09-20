6e2d554da31359c024a5f019728667fddc42b8cb
Siyuan Chen <chansey97@outlook.com>
2021-09-20 20:47:41 +0800

    Update racket-mode (which depended on faceup and pos-tip) in site-lisp.

---
+++ site-lisp/racket-mode-20210721.1526/racket-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "racket-mode" "20210721.1526" "Racket editing, REPL, and more"
+  '((emacs "25.1")
+    (faceup "0.0.2")
+    (pos-tip "20191127.1028"))
+  :commit "e232ca566a6336fc7cffd2b668e0f91f161a7bf0" :authors
+  '(("Greg Hendershott"))
+  :maintainer
+  '("Greg Hendershott")
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
