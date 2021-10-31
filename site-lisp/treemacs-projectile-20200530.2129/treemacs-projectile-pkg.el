a6dffac9708cb5bf30cc85674587bda20582a315
Siyuan Chen <chansey97@outlook.com>
2021-11-01 04:23:54 +0800

    Upload treemacs and treemacs-projectile.
    Source https://github.com/Alexander-Miller/treemacs/tree/ed7de5d0a25b19d9ad5f376b5e447123b418964b

---
+++ site-lisp/treemacs-projectile-20200530.2129/treemacs-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-projectile" "20200530.2129" "Projectile integration for treemacs" '((emacs "25.2") (projectile "0.14.0") (treemacs "0.0")) :commit "ed7de5d0a25b19d9ad5f376b5e447123b418964b" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
