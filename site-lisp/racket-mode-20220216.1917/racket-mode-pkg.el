1e326d2b7bdde2a2907e5ba8310ab6a17d6a2824
Siyuan Chen <chansey97@outlook.com>
2022-02-18 03:42:54 +0800

    Update racket-mode.

---
+++ site-lisp/racket-mode-20220216.1917/racket-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "racket-mode" "20220216.1917" "Racket editing, REPL, and more"
+  '((emacs "25.1"))
+  :commit "22e319754dcf650e282b3ba33b9d0ee3cda81007" :authors
+  '(("Greg Hendershott"))
+  :maintainer
+  '("Greg Hendershott")
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
