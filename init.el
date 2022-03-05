308b0cb7c5a9690e375c990c0cecbf4a995451b4
Siyuan Chen <chansey97@outlook.com>
2022-03-06 03:47:02 +0800

    Improve Scheme settings (file extension, indents, keywords).

--- init.el
+++ init.el
@@ -1084,6 +1084,69 @@ re-downloaded in order to locate PACKAGE."
 ;; scheme-mode
 ;;----------------------------------------------------------------------------
 (require 'cmuscheme)
+(setq auto-mode-alist (cons '("\\.ss" . scheme-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.sls" . scheme-mode) auto-mode-alist))
+
+;; properly indent certain Scheme special forms
+(put 'cond 'scheme-indent-function 0)
+(put 'pmatch 'scheme-indent-function 1)
+(put 'match 'scheme-indent-function 1)
+(put 'dmatch 'scheme-indent-function 1)
+(put 'let-values 'scheme-indent-function 1)
+(put 'call-with-values 'scheme-indent-function 2)
+(put 'syntax-case 'scheme-indent-function 2)
+(put 'test 'scheme-indent-function 1)
+(put 'test-check 'scheme-indent-function 1)
+(put 'test-divergence 'scheme-indent-function 1)
+(put 'make-engine 'scheme-indent-function 0)
+(put 'trace-lambda 'scheme-indent-function 1)
+;; (put 'if 'scheme-indent-function 1)
+(put 'and 'scheme-indent-function 0)
+
+;;; miniKanren-specific indentation
+(put 'lambdaf@ 'scheme-indent-function 1)
+(put 'lambdag@ 'scheme-indent-function 1)
+(put 'fresh 'scheme-indent-function 1)
+(put 'exists 'scheme-indent-function 1)
+(put 'exist 'scheme-indent-function 1)
+(put 'nom 'scheme-indent-function 1)
+(put 'run 'scheme-indent-function 2)
+(put 'conde 'scheme-indent-function 0)
+(put 'conda 'scheme-indent-function 0)
+(put 'condu 'scheme-indent-function 0)
+(put 'project 'scheme-indent-function 1)
+(put 'run* 'scheme-indent-function 1)
+(put 'case-inf 'scheme-indent-function 1)
+
+;;; font-lock for additional Scheme keywords
+;;; http://www.emacswiki.org/emacs/AddKeywords
+(defun scheme-add-keywords (face-name keyword-rules)
+  (let* ((keyword-list (mapcar #'(lambda (x)
+				   (symbol-name (cdr x)))
+			       keyword-rules))
+	 (keyword-regexp (concat "(\\("
+				 (regexp-opt keyword-list)
+				 "\\)[ \n]")))
+    (font-lock-add-keywords 'scheme-mode
+			    `((,keyword-regexp 1 ',face-name)))))
+
+(scheme-add-keywords
+ 'font-lock-keyword-face
+ '((1 . fresh)
+   (1 . conde)
+   (1 . run)
+   (1 . run*)
+   (1 . match)
+   (1 . pmatch)
+   (1 . dmatch)
+   (1 . syntax-case)
+   (1 . with-syntax)
+   (1 . unless)
+   (1 . when)
+   (1 . library)
+   (1 . export)
+   (1 . import)
+   ))
 
 ;; Copy from cmuscheme and override run-scheme, using switch-to-buffer-other-window
 ;; instead of pop-to-buffer-same-window.
@@ -1385,4 +1448,5 @@ re-downloaded in order to locate PACKAGE."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
 
