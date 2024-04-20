7d8b2a8f342e5a2948bcf7fd541f9bf018ff6c77
Siyuan Chen <chansey97@outlook.com>
2024-04-20 09:32:33 +0800

    Update code.

--- site-lisp/sc/font-settings.el
+++ site-lisp/sc/font-settings.el
@@ -59,6 +59,7 @@
 
 ;; All defined character set names
 ;; ELISP> charset-list
+;; (chinese-cns11643-15 gb18030 gb18030-4-byte-ext-2 gb18030-4-byte-ext-1 gb18030-4-byte-smp gb18030-4-byte-bmp gb18030-2-byte cp154 pt154 ptcp154 mik cp850 ibm850 symbol adobe-standard-encoding hp-roman8 cp1047 ibm1047 cp038 ebcdic-int ibm038 ebcdic-uk ebcdic-us mac-roman ethiopic unicode-ssp unicode-sip unicode-smp unicode-bmp mule-unicode-0100-24ff mule-unicode-e000-ffff mule-unicode-2500-33ff tibetan-1-column tibetan indian-2-column indian-1-column indian-glyph malayalam-akruti kannada-akruti telugu-akruti tamil-akruti oriya-akruti gujarati-akruti punjabi-akruti bengali-akruti devanagari-akruti punjabi-cdac gujarati-cdac malayalam-cdac kannada-cdac oriya-cdac assamese-cdac telugu-cdac tamil-cdac bengali-cdac sanskrit-cdac devanagari-cdac indian-is13194 mule-lao lao arabic-2-column arabic-1-column arabic-digit cp874 cp869 cp865 cp864 cp863 cp862 cp861 cp860 cp00858 cp858 cp857 cp855 cp852 cp851 cp775 cp737 cp720 cp437 cp866u ruscii cp1125 next cp1258 windows-1258 cp1257 windows-1257 cp1256 windows-1256 cp1255 windows-1255 cp1254 windows-1254 cp1253 windows-1253 cp1252 windows-1252 cp1251 windows-1251 cp1250 windows-1250 georgian-academy georgian-ps koi8-t koi8-u ibm866 cp866 alternativnyj koi8 koi8-r vscii-2 tcvn-5712 vscii vietnamese-viscii-upper vietnamese-viscii-lower viscii ipa chinese-sisheng cp949 cp949-2-byte big5-hkscs korean-ksc5601 cp932 cp932-2-byte katakana-sjis japanese-jisx0213\.2004-1 japanese-jisx0213-a japanese-jisx0213-2 japanese-jisx0213-1 japanese-jisx0212 japanese-jisx0208-1978 japanese-jisx0208 chinese-big5-2 chinese-big5-1 big5 chinese-cns11643-7 chinese-cns11643-6 chinese-cns11643-5 chinese-cns11643-4 chinese-cns11643-3 chinese-cns11643-2 chinese-cns11643-1 windows-936 cp936 chinese-gbk chinese-gb2312 katakana-jisx0201 latin-jisx0201 jisx0201 tis620-2533 thai-tis620 latin-iso8859-16 iso-8859-16 latin-iso8859-15 iso-8859-15 latin-iso8859-14 iso-8859-14 latin-iso8859-13 iso-8859-13 thai-iso8859-11 iso-8859-11 latin-iso8859-10 iso-8859-10 latin-iso8859-9 iso-8859-9 hebrew-iso8859-8 iso-8859-8 greek-iso8859-7 iso-8859-7 arabic-iso8859-6 iso-8859-6 cyrillic-iso8859-5 iso-8859-5 latin-iso8859-4 iso-8859-4 latin-iso8859-3 iso-8859-3 latin-iso8859-2 iso-8859-2 eight-bit-graphic eight-bit-control control-1 latin-iso8859-1 ucs eight-bit emacs unicode iso-8859-1 ascii)
 
 ;; All defined character set names ordered by their priority
 ;; ELISP> (charset-priority-list)
@@ -230,6 +231,30 @@
 
 (set-face-attribute 'default nil :font (font-spec :family "Courier New" :size 14))
 
+
+;; ## Final Fallback Fonts (speed up font search)
+
+;; TODO: Ideally, this piece of code should be intuitive at the end of file with 'append, but 'append doesn't work!
+;; See https://emacs.stackexchange.com/questions/81062/the-append-argument-for-set-fontset-font-doesnt-work
+;; So I had to put this code at the beginning.
+
+;; "Last Resort"
+(set-fontset-font t 'unicode (font-spec :family "Last Resort" :size 24) nil nil)
+
+;; "Unicode BMP Fallback SIL"
+(set-fontset-font t 'unicode-bmp (font-spec :family "Unicode BMP Fallback SIL" :size 24) nil 'prepend)
+
+;; "Arial Unicode MS"
+;; The font is from Office 2010 and it almost support full BMP (38917 characters)
+;; https://web.archive.org/web/20121122124125/http://msdn.microsoft.com/en-us/goglobal/bb688134.aspx
+;; https://learn.microsoft.com/en-us/typography/font-list/arial-unicode-ms
+;; It is almost, because there are symbols are not in it, e.g. 'Miscellaneous Symbols and Arrow' and 'Miscellaneous Symbols and Pictographs'
+(set-fontset-font t 'unicode-bmp (font-spec :family "Arial Unicode MS" :size 14) nil 'prepend)
+
+;; ภ
+;; 𐁐𐂐𐓣𐔆𐕆𐖆𐙦𐞧𒓇𒿘𗹘𛉚𝥘𝼆𞁙𞻩𭯦𰀴󠅉􍖩󿌋
+
+
 ;; ## Alternative Font
 
 ;; Note that set-fontset-font can not override character ranges in Main font, see
@@ -253,7 +278,7 @@
 ;; CJK 通用, 汉字, 注音符号, 平假名 & 片假名, 漢文(日), 表意符号
 ;; I guess cjk-misc is something like common script (Zyyy) for CJK characters
 (dolist (script '(cjk-misc han bopomofo kana kanbun ideographic-description))
-  (set-fontset-font t script (font-spec :family "Microsoft YaHei" :size 14) nil nil))
+  (set-fontset-font t script (font-spec :family "Microsoft YaHei" :size 14) nil 'prepend))
 ;; cjk-misc: 〄 、
 ;; han: 你好
 ;; bopomofo: ㄅ
@@ -262,11 +287,11 @@
 ;; ideographic-description:〚〛
 
 ;; 韩文
-(set-fontset-font t 'hangul (font-spec :family "Malgun Gothic" :size 14) nil nil)
+(set-fontset-font t 'hangul (font-spec :family "Malgun Gothic" :size 14) nil 'prepend)
 ;; 가각갂갃간
 
 ;; 彝文
-(set-fontset-font t 'yi (font-spec :family "Microsoft Yi Baiti" :size 16) nil nil)
+(set-fontset-font t 'yi (font-spec :family "Microsoft Yi Baiti" :size 16) nil 'prepend)
 ;; ꒐꒑꒒꒓꒔꒕꒖꒗꒘ ꀁꀂꀃꀄꀅꀆꀇꀈꀀ
 
 
@@ -280,81 +305,81 @@
 ;;                           runic
 ;;                           old-south-arabian shavian syriac ugaritic old-persian
 ;;                           cuneiform cuneiform-numbers-and-punctuation))
-;;  (set-fontset-font t script (font-spec :family "Segoe UI Historic" :size 16) nil nil))
+;;  (set-fontset-font t script (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend))
 
-(set-fontset-font t 'aramaic (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'aramaic (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐡀𐡁𐡂𐡃𐡄𐡅𐡆𐡇𐡈
 
-(set-fontset-font t 'brahmi (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'brahmi (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𑀡𑀢𑀣𑀤𑀥𑀠
 
-(set-fontset-font t 'carian (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'carian (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐋁𐋂𐋃𐋄𐋅𐋀
 
-(set-fontset-font t 'cypriot-syllabary (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'cypriot-syllabary (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐠀𐠁𐠁𐠂𐠂𐠃𐠄𐠅
 
-(set-fontset-font t 'egyptian (font-spec :family "Segoe UI Historic" :size 64) nil nil)
+(set-fontset-font t 'egyptian (font-spec :family "Segoe UI Historic" :size 64) nil 'prepend)
 ;; 𓀀𓀁𓀂𓀃 𓃒𓃓𓃔𓃕𓃖𓃗𓃘𓃙 𓄃𓄄 𓅀𓅁𓅂𓄿 
 
-(set-fontset-font t 'glagolitic (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'glagolitic (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; ⰀⰁⰂⰃⰄ
 
-(set-fontset-font t 'gothic (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'gothic (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐌰𐌱𐌲𐌳𐌴𐌵𐌶𐌷𐌸
 
-(set-fontset-font t 'old-italic (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'old-italic (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐌀𐌁𐌂𐌃
 
-(set-fontset-font t 'kharoshthi (font-spec :family "Segoe UI Historic" :size 20) nil nil)
+(set-fontset-font t 'kharoshthi (font-spec :family "Segoe UI Historic" :size 20) nil 'prepend)
 ;; 𐨐𐨑𐨒𐨓
 
-(set-fontset-font t 'lycian (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'lycian (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐊀𐊁𐊂𐊃𐊄
 
-(set-fontset-font t 'lydian (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'lydian (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐤠𐤡𐤢𐤣𐤤𐤥𐤦
 
-(set-fontset-font t 'meroitic (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'meroitic (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐦠𐦡𐦢𐦣𐦤𐦥𐦦𐦧𐦨
 
-(set-fontset-font t 'ogham (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'ogham (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;;  ᚁᚂᚃᚄᚅ
 
-(set-fontset-font t 'old-turkic (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'old-turkic (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐰀𐰁𐰂𐰃𐰄𐰅
 
-(set-fontset-font t 'inscriptional-pahlavi (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'inscriptional-pahlavi (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐭠𐭡𐭢𐭣𐭤𐭥
 
-(set-fontset-font t 'phoenician (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'phoenician (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐤀𐤁𐤂𐤃𐤄𐤅𐤆𐤇𐤈
 
-(set-fontset-font t 'inscriptional-parthian (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'inscriptional-parthian (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐭀𐭁𐭂𐭃𐭄𐭅𐭆𐭇𐭈
 
-(set-fontset-font t 'runic (font-spec :family "Segoe UI Historic" :size 20) nil nil)
+(set-fontset-font t 'runic (font-spec :family "Segoe UI Historic" :size 20) nil 'prepend)
 ;; ᛋᛗᚪᚢᚷ ᚹᚣᚱᛗ
 
-(set-fontset-font t 'old-south-arabian (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'old-south-arabian (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐩡𐩢𐩣𐩤𐩥𐩠
 
-(set-fontset-font t 'shavian (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'shavian (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; 𐑑𐑒𐑓𐑔𐑕𐑖𐑗𐑘𐑙
 
-(set-fontset-font t 'syriac (font-spec :family "Segoe UI Historic" :size 16) nil nil)
+(set-fontset-font t 'syriac (font-spec :family "Segoe UI Historic" :size 16) nil 'prepend)
 ;; ܐܑܒܓܔ
 
-(set-fontset-font t 'ugaritic (font-spec :family "Segoe UI Historic" :size 20) nil nil)
+(set-fontset-font t 'ugaritic (font-spec :family "Segoe UI Historic" :size 20) nil 'prepend)
 ;; 𐎀𐎁𐎂𐎃𐎄𐎅𐎆
 
-(set-fontset-font t 'old-persian (font-spec :family "Segoe UI Historic" :size 20) nil nil)
+(set-fontset-font t 'old-persian (font-spec :family "Segoe UI Historic" :size 20) nil 'prepend)
 ;; 𐎠𐎡𐎢𐎣𐎤𐎥𐎦
 
-(set-fontset-font t 'cuneiform (font-spec :family "Segoe UI Historic" :size 20) nil nil)
+(set-fontset-font t 'cuneiform (font-spec :family "Segoe UI Historic" :size 20) nil 'prepend)
 ;; 𒀀𒀁𒀂𒀃𒀄𒀅𒀆
 
-(set-fontset-font t 'cuneiform-numbers-and-punctuation (font-spec :family "Segoe UI Historic" :size 20) nil nil)
+(set-fontset-font t 'cuneiform-numbers-and-punctuation (font-spec :family "Segoe UI Historic" :size 20) nil 'prepend)
 ;; 𒐀𒐁𒐂𒐃𒐄𒐅𒐆𒐇𒐈
 
 ;; TODO: Add ligature
@@ -376,7 +401,7 @@
                                        ;; "Symbola"
                                        ;; "DejaVu Math Tex Gyre"
                                        :size 16)
-                  nil nil)
+                  nil 'prepend)
 ;; Mathematical Operators in main font: ∑ √ ∩ ∫ ∆ (only be overidden, if use-default-font-for-symbols is nil)
 ;; Mathematical Operators not in main font: ∀ ∃ ∅ ∉∨
 ;; Miscellaneous Mathematical Symbols-A: ⟦⟧
@@ -387,75 +412,62 @@
                                              ;; "Symbola"
                                              ;; "DejaVu Math Tex Gyre"
                                              :size 16)
-                  nil nil)
+                  nil 'prepend)
 ;; Mathematical Alphanumeric Symbols: 𝓐𝓑𝓒𝓓
 
 
 ;; ### Emojis
-;; Since use-default-font-for-symbols = nil, fontset must use specific ranges to override 
-;; instead of blindly using script 'symbol with prepend.
-;; (set-fontset-font t 'symbol (font-spec :family "Segoe UI Emoji" :size 20) nil 'prepend)
-(require 'sc/emoji)
+(let ((emoji-fs (font-spec :family
+                     ;; "Segoe UI Emoji"
+                     ;; "Noto Emoji"
+                     "OpenMoji" ; TODO: Doesn't work in Emacs < 28?
+                     :size 20)))
+  (cond ((version< emacs-version "28.1")
+         ;; In Emacs < 28.1, there was no 'emoji charset, specific emoji ranges must be used.
+         (require 'sc/emoji)
+         (dolist (charset sc/emoji-charsets)
+           (set-fontset-font t charset emoji-fs nil 'prepend)))
+        (t
+         ;; In Emacs >= 28.1, the 'emoji charset was introduced.
+         ;; https://github.com/emacs-mirror/emacs/blob/master/etc/NEWS.28#L209
+         (set-fontset-font t 'emoji emoji-fs nil 'prepend))))
 
-(dolist (charset sc/emoji-charsets)
-  (set-fontset-font t charset (font-spec :family
-                                         "Segoe UI Emoji"
-                                         ;; "Noto Emoji"
-                                         ;; "OpenMoji" ; TODO: OpenMoji doesn't show, I don't know why
-                                         :size 20)
-                    nil nil))
-;; ⌚⌛🐙😀😂
+;; https://github.com/emacs-mirror/emacs/blob/e5b4d4dd1bb4d568ed20cfb7354c5ff898af7a05/etc/NEWS.28#L209
 
-;; TODO: Add flags
+;; TODO: Add flags and test difference with Emacs >= 28.1
 ;; 1. "Segoe UI Emoji" has no flags, so use "BabelStone Flags" instead
 ;; 2. Emacs does not seem to support combining characters? Maybe unicode-fonts? or Emacs >= 28.1?  
 ;; https://www.masteringemacs.org/article/unicode-ligatures-color-emoji
 ;; (dolist (charset '(#x1F1EF #x1F1F5))
-;;   (set-fontset-font t charset (font-spec :family "BabelStone Flags" :size 20) nil nil))
+;;   (set-fontset-font t charset (font-spec :family "BabelStone Flags" :size 20) nil 'prepend))
 
 
 ;; ### mahjong-tile
-(set-fontset-font t 'mahjong-tile (font-spec :family "Segoe UI Emoji" :size 32) nil nil)
-(set-fontset-font t 'mahjong-tile (font-spec :family "Segoe UI Symbol" :size 32) nil 'append)
+(set-fontset-font t 'mahjong-tile (font-spec :family "Segoe UI Symbol" :size 32) nil 'prepend)
+(set-fontset-font t 'mahjong-tile (font-spec :family "Segoe UI Emoji" :size 32) nil 'prepend)
 ;; 🀀🀁🀂🀃🀄🀅🀆🀇🀈🀉🀊🀋🀌🀍🀎🀏
 
 
 ;; ### domino-tile
-(set-fontset-font t 'domino-tile (font-spec :family "Segoe UI Emoji" :size 32) nil nil)
-(set-fontset-font t 'domino-tile (font-spec :family "Segoe UI Symbol" :size 32) nil 'append)
+(set-fontset-font t 'domino-tile (font-spec :family "Segoe UI Symbol" :size 32) nil 'prepend)
+(set-fontset-font t 'domino-tile (font-spec :family "Segoe UI Emoji" :size 32) nil 'prepend)
 ;; domino-tile: 🁀 🁁 🁂 🁃🁰🁱🁲🁳
 
 
 ;; ### playing-cards
-(set-fontset-font t 'playing-cards (font-spec :family "Segoe UI Emoji" :size 64) nil nil)
-(set-fontset-font t 'playing-cards (font-spec :family "Segoe UI Symbol" :size 64) nil 'append)
+(set-fontset-font t 'playing-cards (font-spec :family "Segoe UI Symbol" :size 64) nil 'prepend)
+(set-fontset-font t 'playing-cards (font-spec :family "Segoe UI Emoji" :size 64) nil 'prepend)
 ;; playing-cards: 🂠🂡🂢🂣🂤🂥🂦🂧🂨🂩🂪🂫🂬🂭🂮
 
 
 ;; ### chess-symbol
-(set-fontset-font t 'chess-symbol (font-spec :family "Segoe UI Emoji" :size 20) nil nil)
-(set-fontset-font t 'chess-symbol (font-spec :family "Noto Sans Symbols 2" :size 20) nil 'append)
+(set-fontset-font t 'chess-symbol (font-spec :family "Noto Sans Symbols 2" :size 20) nil 'prepend)
+(set-fontset-font t 'chess-symbol (font-spec :family "Segoe UI Emoji" :size 20) nil 'prepend)
 ;; chess-symbol: 🨀🨁🨂🨃🨄🨅🩠🩡🩢🩣🩤🩥🩦
 
-
-;; ### Unicode Fallback Fonts (speed up font search)
-
-;; "Arial Unicode MS"
-;; The font is from Office 2010 and it almost support full BMP (38917 characters)
-;; https://web.archive.org/web/20121122124125/http://msdn.microsoft.com/en-us/goglobal/bb688134.aspx
-;; https://learn.microsoft.com/en-us/typography/font-list/arial-unicode-ms
-;; It is almost, because there are symbols are not in it, e.g. 'Miscellaneous Symbols and Arrow' and 'Miscellaneous Symbols and Pictographs'
-(set-fontset-font t 'unicode-bmp (font-spec :family "Arial Unicode MS" :size 14) nil 'append) ; from
-
-;; "Unicode BMP Fallback SIL"
-(set-fontset-font t 'unicode-bmp (font-spec :family "Unicode BMP Fallback SIL" :size 24) nil 'append)
-
-;; "Last Resort"
-(dolist (script '(unicode unicode-smp unicode-sip unicode-ssp))
-  (set-fontset-font t script (font-spec :family "Last Resort" :size 24) nil 'append))
-
-
 (provide 'sc/font-settings)
+
+
 
 
 
