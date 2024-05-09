59f13936e1335ed2da14a121ce1e378c40a8ba43
Siyuan Chen <chansey97@outlook.com>
2024-05-09 15:10:04 +0800

    Install rg.el and move it and its deps to site-lisp.

---
+++ site-lisp/transient-20240506.1559/transient-pkg.el
@@ -0,0 +1,16 @@
+(define-package "transient" "20240506.1559" "Transient commands"
+  '((emacs "26.1")
+    (compat "29.1.4.4")
+    (seq "2.24"))
+  :commit "753de9088e0f71ed57120f4e42e81430ab9e48d6" :authors
+  '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")
+  :keywords
+  '("extensions")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
