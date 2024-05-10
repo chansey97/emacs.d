e4963b2aee4be1643cbf43d26c530c4bf4acd887
Siyuan Chen <chansey97@outlook.com>
2024-05-10 22:52:13 +0800

    Add new sc-search using transient menu.

--- init.el
+++ init.el
@@ -1042,12 +1042,25 @@ unmodified snippet field.")
 ;;----------------------------------------------------------------------------
 (require 'rg)
 (setq rg-executable "C:/env/ripgrep/ripgrep-14.1.0-x86_64-pc-windows-msvc/rg.exe")
-(setenv "RIPGREP_CONFIG_PATH" "C:/env/ripgrep/config/.ripgreprc")
-;; (setq rg-command-line-flags '("--encoding=utf-8"))
-;; (setq rg-command-line-flags '("--ignore-file=C:/Users/Chansey/AppData/Roaming/.emacs.d/你好世界/test.txt")) ; path has unicode is support
 
 (when (eq system-type 'windows-nt)
   (setq rg-w32-unicode t))
+
+;; Note: Using the ripgrep configuration file may break functionality of this package if you are not careful.
+;; (setenv "RIPGREP_CONFIG_PATH" "C:/env/ripgrep/config/.ripgreprc")
+;; (setq rg-ignore-ripgreprc nil)
+
+;; (setq rg-command-line-flags '("--encoding=utf-8"))
+;; (setq rg-command-line-flags '("--ignore-file=C:/Users/Chansey/AppData/Roaming/.emacs.d/你好世界/test.txt")) ; path has unicode is support
+
+(transient-define-prefix sc-search ()
+    "A menu to do various searches"
+    [["Various Searches"
+      ("1" "rg" rg)
+      ("2" "rg-project" rg-project)
+      ("3" "rg-menu" rg-menu)
+      ("4" "projectile--find-file" projectile--find-file)]])
+(global-set-key (kbd "C-S-f") 'sc-search)
 
 ;; Just for debugging rg-w32-unicode feature, no need to exist after finish.
 ;; Keep it here as an example of start-process.
