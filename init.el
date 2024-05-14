de7b853f29e76bc0a28d8d0055b985eaf67daced
Siyuan Chen <chansey97@outlook.com>
2024-05-14 16:30:21 +0800

    Add some comment for define-advice show-paren-function

--- init.el
+++ init.el
@@ -287,7 +287,8 @@
 ;; turn on bracket match highlight
 (show-paren-mode 1)
 
-;; highlight enclosing parens
+;; highlight enclosing parens instead of out
+;; https://stackoverflow.com/questions/22770979/parenthesis-highlighting-emacs
 (define-advice show-paren-function (:around (fn) fix-show-paren-function)
   "Highlight enclosing parens."
   (cond ((looking-at-p "\\s(") (funcall fn))
