772a02c77c87c3c3e6b3cec034df0319902821de
Siyuan Chen <chansey97@outlook.com>
2024-05-18 22:53:30 +0800

    Add purpose of sc-set-server

--- lisp/sc-set/sc-set-server.el
+++ lisp/sc-set/sc-set-server.el
@@ -2,7 +2,20 @@
 ;; Server
 ;;----------------------------------------------------------------------------
 (require 'server)
-(unless (server-running-p)
+
+;; Passing files from obsidian to Emacs
+;; 1. Install "Open with Plugin" in Obsidian
+;; Add Emacsclientw C:\green\emacs-sh\emacsclientw.lnk
+;; 2. Create emacsclientw.cmd in C:\green\emacs-sh
+;; ```
+;; @echo off
+;; start emacsclientw.exe %1
+;; exit
+;; ```
+;; 3. Create emacsclientw.lnk
+;; Properties -> Shortcut -> Run -> Minimized
+
+(unless server-process
   (server-start))
 
 (provide 'sc-set-server)
